49c3379b8c90619e00ca063cdb9d65fc7c843fec
devna <devna@DESKTOP-O3A9TKC>
2018-04-08 01:56:23 +0530

    This is for chck

--- AutomationTest/src/thepackage/MyClass.java
+++ AutomationTest/src/thepackage/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 	public static void main(String[]Dev) {
 		
 		System.out.println("I can do it .... I will be the best one  ");
+		System.out.println("I am the Best");
 		
 	}
 	
